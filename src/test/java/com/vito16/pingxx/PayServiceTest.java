8595dd697219d8d3f18d5a2782acf69f1f6473f4
Vito Zhou <zhouwentao16@gmail.com>
2016-04-26 13:34:46 +0800

    修改单元测试代码

--- src/test/java/com/vito16/pingxx/PayServiceTest.java
+++ src/test/java/com/vito16/pingxx/PayServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.fastjson.JSONObject;
 import com.vito16.pingxx.order.PayOrder;
 import com.vito16.pingxx.service.PayService;
 
@@ -25,13 +26,13 @@ public class PayServiceTest {
     @Test
     public void testPay(){
         PayOrder order = new PayOrder();
-        order.setAmount(21);
+        order.setAmount(9900);
         order.setBody("XXX.X..X.XX.=SDFJKSDJF");
         order.setChannel("alipay");
         order.setClientIp("192.168.23.23");
         order.setOrderNo("2341234223");
         order.setSubject("保健品");
         payService.pay(order);
-        logger.info("order:{}",order);
+        logger.info("order:{}", JSONObject.toJSONString(order));
     }
 }
